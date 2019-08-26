fbd857dd1a1893cb377c19c6aa412910709208c4
Edmund Home <edmundhome@hotmail.com>
2019-08-26 17:00:05 +0200

    Empezamos el puto proyecto, nos vamos a inchar a follar

---
+++ src/main/java/com/lucatinder/g3/LucaTinderApplication.java
@@ -0,0 +1,13 @@
+package com.lucatinder.g3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucaTinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucaTinderApplication.class, args);
+	}
+
+}
